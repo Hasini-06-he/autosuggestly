dfa4313a4b6108a59458286fabbe2b1fed60d82d
Hasini Sai Jallaram <hasinisaijallaram@gmail.com>
2026-07-03 13:42:12 +0530

    comment

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express') 
+
+const app = express()
+
+// change this
+const PORT = process.env.PORT || 3002
+
+//webserver :
+app.use(express.static("frontend"))
+
+app.listen(PORT, function(){
+  console.log("Server is running at http://localhost:" + PORT)
+})
